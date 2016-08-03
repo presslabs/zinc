af937dafc6a5e0ed4197026466203fb89a584b64
Krody Robert <krody.robi@gmail.com>
2016-08-03 11:44:16 +0300

    Initial structure

---
+++ ips/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IpsConfig(AppConfig):
+    name = 'ips'
---
+++ zones/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ZonesConfig(AppConfig):
+    name = 'zones'
