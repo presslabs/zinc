b469371786657534b7a6fed00b95dc077985cf5c
Krody Robert <krody.robi@gmail.com>
2016-08-03 14:19:23 +0300

    Removed records app as it is tied to a zone

--- records/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecordsConfig(AppConfig):
-    name = 'records'
--- zinc/settings.py
+++ zinc/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'domains',
     'ips',
     'policies',
-    'records',
     'zones'
 ]
 
