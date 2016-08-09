6a051df5c50f26ad0f7ba9bbce1ac6c8eaab1812
ionut <ionut@presslabs.com>
2016-08-09 14:24:23 +0300

    Remove unnecessary validator

--- dns/utils.py
+++ dns/utils.py
@@ -1,14 +1,5 @@
-from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
-from django.utils.translation import ugettext_lazy as _
 
-
-def validate_positive(value):
-    if value < 0:
-        raise ValidationError(
-            _('%(value)s is not an positive number'),
-            params={'value': value},
-        )
 
 validate_hostname = RegexValidator(
     regex=r'^(?=[a-z0-9\-\.]{1,253}$)([a-z0-9](([a-z0-9\-]){,61}[a-z0-9])?\.)*([a-z0-9](([a-z0-9\-]){,61}[a-z0-9])?)$',
