cb78e8aeb2bca270d3217a00aacf968be190ab07
ionut <ionut@presslabs.com>
2016-08-10 15:40:07 +0300

    Don't use reserved keywords for var names

--- dns/serializers/record.py
+++ dns/serializers/record.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 
 
 class RecordSerializer(serializers.Serializer):
-    RECORD_TYPES = [(type, type) for type in ['A', 'AAAA', 'CNAME', 'MX', 'TXT', 'SPF', 'SRV', 'NS', 'SOA']]
+    RECORD_TYPES = [(rtype, rtype) for rtype in ['A', 'AAAA', 'CNAME', 'MX', 'TXT', 'SPF', 'SRV', 'NS', 'SOA']]
 
-    type = serializers.ChoiceField(choices=RECORD_TYPES)
+    record_type = serializers.ChoiceField(choices=RECORD_TYPES)
     name = serializers.CharField(max_length=255)
     value = serializers.CharField()
     ttl = serializers.IntegerField(min_value=300)
