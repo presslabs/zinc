a119b96a1b5e05fc599899f5618b36114159e63b
ionut <ionut@presslabs.com>
2016-08-11 17:52:07 +0300

    Fix AWS region retrieval method

--- dns/utils.py
+++ dns/utils.py
@@ -3,4 +3,4 @@ from boto3.session import Session
 
 def get_regions():
     '''Retrieve a list of region tuples available in AWS EC2'''
-    return [(r.name, r.name) for r in Session().get_available_regions('ec2')]
+    return [(region, region) for region in Session().get_available_regions('ec2')]
