870287ebfaa5a14d57fbf9b77d6e19ebc87f975c
Krody Robert <krody.robi@gmail.com>
2016-08-11 12:08:05 +0300

    Celery

--- zinc/settings.py
+++ zinc/settings.py
@@ -133,6 +133,14 @@ LATTICE_URL = 'https://lattice.presslabs.net/'
 LATTICE_USER = ''
 LATTICE_PASS = ''
 
+# CELERY
+
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
 # =================
 # END OF CONFIG
 
