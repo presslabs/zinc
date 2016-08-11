a13c001e7f9fc258e5e447e8de1995a0594c340d
ionut <ionut@presslabs.com>
2016-08-11 17:49:20 +0300

    Add custom create and update methods on record serializer

--- dns/serializers/record.py
+++ dns/serializers/record.py
@@ -1,11 +1,36 @@
 from rest_framework import serializers
 
+from ..models import ManagedRecord
+
 
 class RecordSerializer(serializers.Serializer):
-    RECORD_TYPES = [(rtype, rtype) for rtype in ['A', 'AAAA', 'CNAME', 'MX', 'TXT', 'SPF', 'SRV', 'NS', 'SOA']]
+    RECORD_TYPES = ['A', 'AAAA', 'CNAME', 'MX', 'TXT', 'SPF', 'SRV', 'NS', 'SOA', 'POLICY_ROUTED']
 
-    record_type = serializers.ChoiceField(choices=RECORD_TYPES)
     name = serializers.CharField(max_length=255)
+    record_type = serializers.ChoiceField(choices=[(rtype, rtype) for rtype in RECORD_TYPES])
     value = serializers.CharField()
     ttl = serializers.IntegerField(min_value=300)
     managed = serializers.BooleanField(default=False)
+    dirty = serializers.BooleanField(default=False, editable=False)
+
+    def create(self, validated_data):
+        validated_data['dirty'] = True
+        rtype = validated_data.get('record_type')
+
+        if rtype == 'POLICY_ROUTED':
+            validated_data['managed'] = True
+            ManagedRecord.objects.all().get_or_create(**validated_data)
+
+        # TODO AWS stuff
+
+    def update(self, instance, validated_data):
+        rtype = validated_data.get('record_type')
+        if rtype == 'POLICY_ROUTED':
+            instance.save()
+
+        # TODO AWS stuff
+
+    class Meta:
+        model = ManagedRecord
+        fields = ('name', 'record_type', 'value', 'ttl', 'dirty')
+        read_only_fields = ('dirty')
