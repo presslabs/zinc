d02cfaacf7d6ba70d81a009a897e362e8afa107d
ionut <ionut@presslabs.com>
2016-08-16 15:54:41 +0300

    Better indent

--- dns/models/managed_record.py
+++ dns/models/managed_record.py
@@ -4,7 +4,11 @@ from django.db import models
 
 class ManagedRecord(models.Model):
     name = models.CharField(max_length=255)
-    record_type = models.CharField(default='POLICY_ROUTED', max_length=13, editable=False)
+    record_type = models.CharField(
+        default='POLICY_ROUTED',
+        max_length=13,
+        editable=False
+    )
     value = models.TextField()
     ttl = models.IntegerField(validators=[MinValueValidator(300)])
     dirty = models.BooleanField(default=True, editable=False)
