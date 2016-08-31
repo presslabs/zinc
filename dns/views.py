9ec9ce1cccc4800383b9ce1ac78de1b09ea7205e
Krody Robert <krody.robi@gmail.com>
2016-08-31 13:53:19 +0300

    Added update/destroy view only

--- dns/views.py
+++ dns/views.py
@@ -5,6 +5,11 @@ from dns.serializers import (PolicySerializer, RecordSerializer,
                              ZoneSerializer, ZoneDetailSerializer)
 
 
+class UpdateDestroyApiView(generics.CreateAPIView, generics.DestroyAPIView,
+                           generics.UpdateAPIView):
+    pass
+
+
 class ZoneList(generics.ListCreateAPIView):
     queryset = Zone.objects.all()
     serializer_class = ZoneSerializer
@@ -29,6 +34,6 @@ class PolicyDetail(generics.RetrieveUpdateAPIView):
         return Policy.objects.filter(id=self.kwargs['pk'])
 
 
-class RecordList(generics.ListCreateAPIView):
+class RecordList(UpdateDestroyApiView):
     queryset = PolicyRecord.objects.all()
     serializer_class = RecordSerializer
