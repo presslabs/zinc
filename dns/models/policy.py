d0dd80e6c91c91a03e03f5c799369d49fd4d8c29
Krody Robert <krody.robi@gmail.com>
2016-11-02 18:05:52 +0200

    Added abstraction over memberships

--- dns/models/policy.py
+++ dns/models/policy.py
@@ -1,12 +1,9 @@
 from django.db import models
-from django.db.models.signals import pre_save
-
 from dns.models import PolicyMember
 
 
 class Policy(models.Model):
     name = models.CharField(max_length=255, unique=True, null=False)
-    modified_index = models.PositiveIntegerField(default=0, editable=False)
     members = models.ManyToManyField(PolicyMember)
 
     def __str__(self):
@@ -15,12 +12,5 @@ class Policy(models.Model):
     def __unicode__(self):
         return self.__str__()
 
-    @staticmethod
-    def modify_index(sender, instance, *args, **kwargs):
-        instance.modified_index += 1
-
     class Meta:
         verbose_name_plural = 'policies'
-
-
-pre_save.connect(Policy.modify_index, sender=Policy)
