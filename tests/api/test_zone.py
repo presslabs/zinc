acb502c6d0a12fc600ea602135fe362384008f6d
Radu Ciorba <radu@devrandom.ro>
2017-01-17 16:49:52 +0200

    allow running test with both fake and boto client

--- tests/api/test_zone.py
+++ tests/api/test_zone.py
@@ -1,9 +1,10 @@
 import pytest
+from mock import patch
 
 from rest_framework.test import APIClient
 
 from dns import models as m
-from dns.utils.route53 import client
+from dns.utils import route53
 
 
 @pytest.fixture
@@ -11,8 +12,39 @@ def api_client():
     return APIClient()
 
 
+class Moto:
+    """"Mock boto"""
+    def create_hosted_zone(self, **kwa):
+        return {
+            'HostedZone': {
+                'Id': 'Fake/Fake/Fake'
+            }
+        }
+
+    def delete_hosted_zone(self, Id):
+        pass
+
+@pytest.fixture(
+    # scope='module',
+    params=[
+        Moto,
+        lambda: route53.client,
+    ],
+    ids=['fake_boto', 'with_aws']
+)
+def boto_client(request):
+    client = request.param()
+    patcher = patch('dns.utils.route53.client', client)
+    patcher.start()
+    def cleanup():
+        patcher.stop()
+    request.addfinalizer(cleanup)
+    return client
+
+
+
 @pytest.mark.django_db
-def test_create_zone(api_client):
+def test_create_zone(api_client, boto_client):
     root = 'example.com.presslabs.com'
     resp = api_client.post(
         '/zones/',
@@ -26,4 +58,4 @@ def test_create_zone(api_client):
     assert list(m.Zone.objects.all().values_list('id', 'root')) == [(_id, root)]
 
     #TODO: this needs to move to fixture cleanup
-    client.delete_hosted_zone(Id=m.Zone.objects.first().route53_id)
+    boto_client.delete_hosted_zone(Id=m.Zone.objects.first().route53_id)
