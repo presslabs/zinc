9a92323945ff7329d282705e05b98c6569587ac8
Radu Ciorba <radu@devrandom.ro>
2017-01-26 16:11:08 +0200

    remove unused celery_once library

--- zinc/vendors/celery.py
+++ zinc/vendors/celery.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery
-from celery_once import QueueOnce
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zinc.settings')
