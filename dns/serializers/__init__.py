cd3f5fdb44badac6d720dd12040d8879813646ec
amecea <f.mecea@yahoo.ro>
2017-02-01 16:19:15 +0200

    Merge RecordSetSerializer it with ZoneSerializer.

--- dns/serializers/__init__.py
+++ dns/serializers/__init__.py
@@ -1,3 +1,3 @@
 from dns.serializers.policy import PolicySerializer, PolicyMemberSerializer
-from dns.serializers.record import RecordSetSerializer, RecordSerializer
+from dns.serializers.record import RecordSerializer
 from dns.serializers.zone import ZoneListSerializer, ZoneDetailSerializer
