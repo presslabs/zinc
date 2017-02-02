9a4874629aec122b85733268fb7c9bcd76aa0ea3
amecea <f.mecea@yahoo.ro>
2017-02-02 15:59:32 +0200

    Fix urls for policy member and add some policy member tests.

--- tests/api/test_policy.py
+++ tests/api/test_policy.py
@@ -6,12 +6,15 @@ from tests.fixtures import api_client, boto_client, zone
 from dns import models as m
 
 
+def policy_member_to_dict(member):
+    return 'http://testserver/policy-members/{}/'.format(member.id)
+
+
 def policy_to_dict(policy):
-    assert policy.members.all().count() == 0
     return {
         'id': str(policy.id),
         'name': policy.name,
-        'members': [],
+        'members': [policy_member_to_dict(member) for member in policy.members.all()],
         'url': 'http://testserver/policies/{}/'.format(policy.id)
     }
 
@@ -38,3 +41,62 @@ def test_policy_create(api_client):
     assert resp.status_code == 201, resp.data
     policy, = list(m.Policy.objects.all())
     assert resp.data == policy_to_dict(policy)
+
+
+@pytest.mark.django_db
+def test_policy_details(api_client):
+    policy = G(m.Policy)
+    response = api_client.get(
+        '/policies/%s/' % policy.id,
+        format='json',
+    )
+    assert response.status_code == 200, response
+    assert response.data == policy_to_dict(policy)
+
+
+@pytest.mark.django_db
+def test_policy_with_records(api_client):
+    policy = G(m.Policy)
+    G(m.PolicyMember, policy=policy)
+    G(m.PolicyMember, policy=policy)
+    response = api_client.get(
+        '/policies/%s/' % policy.id,
+        format='json',
+    )
+
+    assert response.status_code == 200, response
+    assert response.data == policy_to_dict(policy)
+
+@pytest.mark.django_db
+def test_policy_deletion(api_client):
+    policy = G(m.Policy)
+    G(m.PolicyMember, policy=policy)
+    G(m.PolicyMember, policy=policy)
+    assert m.PolicyMember.objects.count() == 2
+
+    response = api_client.delete(
+        '/policies/%s/' % policy.id,
+        format='json',
+    )
+
+    assert response.status_code == 200, response
+    assert m.PolicyMember.objects.count() == 0
+    assert m.Policy.objects.count() == 0
+
+def record_to_dict(record):
+    return {
+        'id': str(record.id),
+        'location': record.location,
+        'ip': record.ip.ip,
+        'weight': record.weight,
+    }
+
+@pytest.mark.django_db
+def test_policy_member(api_client):
+    member = G(m.PolicyMember)
+    response = api_client.get(
+        '/policy-members/%s/' % member.id,
+    )
+
+    assert response.status_code == 200, response
+    assert response.data == record_to_dict(member)
