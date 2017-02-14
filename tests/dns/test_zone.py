90b2ef1af9511ec74d600697efe171e468cea4b7
Radu Ciorba <radu@devrandom.ro>
2017-02-14 18:08:45 +0200

    add test for zone deletion

--- tests/dns/test_zone.py
+++ tests/dns/test_zone.py
@@ -1,10 +1,11 @@
 # pylint: disable=no-member,protected-access,redefined-outer-name
+import botocore.exceptions
 import pytest
 from django_dynamic_fixture import G
 
-from tests.fixtures import boto_client, zone
+from dns import models as m
 from dns.utils.route53 import get_local_aws_regions
-
+from tests.fixtures import boto_client, zone  # pylint: disable=unused-import
 
 regions = get_local_aws_regions()
 
@@ -88,3 +89,48 @@ def test_delete_zone_alias_record_with_set_id(zone):
     zone.save()
 
     assert record_hash not in zone.records
+
+
+@pytest.mark.django_db
+def test_zone_delete(zone):
+    zone, client = zone
+    zone_id = zone.route53_zone.id
+    zone_name = 'test-zinc.net.'
+    client.change_resource_record_sets(
+        HostedZoneId=zone_id,
+        ChangeBatch={
+            'Comment': 'zinc-fixture',
+            'Changes': [
+                {
+                    'Action': 'CREATE',
+                    'ResourceRecordSet': {
+                        'Name': 'some_ns.%s' % zone_name,
+                        'Type': 'NS',
+                        'TTL': 300,
+                        'ResourceRecords': [
+                            {
+                                'Value': 'ns-1941.awsdns-50.co.uk.',
+                            }
+                        ]
+                    }
+                },
+                {
+                    'Action': 'CREATE',
+                    'ResourceRecordSet': {
+                        'Name': 'some_a.%s' % zone_name,
+                        'Type': 'A',
+                        'TTL': 300,
+                        'ResourceRecords': [
+                            {
+                                'Value': '1.1.1.2',
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    )
+    zone.route53_zone.delete()
+    with pytest.raises(botocore.exceptions.ClientError) as excp:
+        client.get_hosted_zone(Id=zone_id)
+    assert excp.value.response['Error']['Code'] == 'NoSuchHostedZone'
