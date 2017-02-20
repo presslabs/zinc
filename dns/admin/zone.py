a620a889b534793956e80256b67a198dbc943700
Radu Ciorba <radu@devrandom.ro>
2017-02-20 19:36:15 +0200

    nicer zone listing in admin

--- dns/admin/zone.py
+++ dns/admin/zone.py
@@ -5,7 +5,7 @@ from dns.models import Zone
 
 @admin.register(Zone)
 class ZoneAdmin(admin.ModelAdmin):
-    list_display = ['root', 'deleted']
+    list_display = ['root', 'is_deleted', 'aws_link']
     readonly_fields = ['route53_id', 'caller_reference']
     list_filter = ['deleted']
 
@@ -24,3 +24,14 @@ class ZoneAdmin(admin.ModelAdmin):
     def delete_model(self, request, obj):
         """Soft delete zone object"""
         obj.soft_delete()
+
+    def is_deleted(self, obj):
+        return "DELETED" if obj.deleted else ""
+
+    def aws_link(self, obj):
+        if obj.route53_id is not None:
+            return ('<a href="https://console.aws.amazon.com/route53/home?region=us-east-1'
+                    '#resource-record-sets:{0}">AWS:{0}</a>'.format(obj.route53_id))
+        else:
+            return ""
+    aws_link.allow_tags = True
