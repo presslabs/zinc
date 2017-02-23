11d7ac3230fbfe209347426fbeec6001593c87c0
Radu Ciorba <radu@devrandom.ro>
2017-02-23 14:33:00 +0200

    add periodic task for lattice sync

---
+++ lattice_sync/tasks.py
@@ -0,0 +1,16 @@
+from celery.utils.log import get_task_logger
+from celery import shared_task
+from django.conf import settings
+
+from dns.utils import route53
+from dns import models
+from lattice_sync import sync
+
+logger = get_task_logger(__name__)
+
+
+@shared_task(ignore_result=True, default_retry_delay=60)
+def lattice_sync():
+    lattice = sync.lattice_factory(
+        settings.LATTICE_URL, settings.LATTICE_USER, settings.LATTICE_PASSWORD)
+    sync.sync(lattice)
