4d1c3759db1dd4564a7ea9ef9f09c179cfc5576d
amecea <f.mecea@yahoo.ro>
2017-02-28 16:30:43 +0200

    Add in fixture force authenticate.

--- zinc/settings.py
+++ zinc/settings.py
@@ -166,6 +166,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
