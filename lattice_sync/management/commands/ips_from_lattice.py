c1ee8bd873fe056c604b61c3946ae317e0ca1245
Radu Ciorba <radu@devrandom.ro>
2017-02-28 14:42:34 +0200

    update command defaults and remove superfluous test

--- lattice_sync/management/commands/ips_from_lattice.py
+++ lattice_sync/management/commands/ips_from_lattice.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 from lattice_sync import sync
 
@@ -12,9 +13,9 @@ class Command(BaseCommand):
     help = 'Imports IPs from a lattice server'
 
     def add_arguments(self, parser):
-        parser.add_argument('--url', default='')
-        parser.add_argument('--user', default='')
-        parser.add_argument('--password', default='')
+        parser.add_argument('--url', default=settings.LATTICE_URL)
+        parser.add_argument('--user', default=settings.LATTICE_USER)
+        parser.add_argument('--password', default=settings.LATTICE_PASSWORD)
 
     def handle(self, *args, **options):
         lattice = sync.lattice_factory(options['url'],
