3fb32bab2159a990dcad32272c30f37065b7e7de
Calin Don <calin.don@gmail.com>
2017-03-03 14:51:01 +0200

    Moved top level resources to viewsets

--- dns/serializers/policy.py
+++ dns/serializers/policy.py
@@ -2,22 +2,18 @@ from rest_framework import serializers
 from dns.models import Policy, PolicyMember
 
 
-class PolicySerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.CharField(read_only=True)
-    members = serializers.HyperlinkedRelatedField(
-        many=True,
-        view_name='policy-member-detail',
-        queryset=PolicyMember.objects.all()
-    )
-
-    class Meta:
-        model = Policy
-        fields = ['id', 'name', 'members', 'url']
-
-
 class PolicyMemberSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True)
 
     class Meta:
         model = PolicyMember
         fields = ['id', 'region', 'ip', 'weight']
+
+
+class PolicySerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.CharField(read_only=True)
+    members = PolicyMemberSerializer(many=True)
+
+    class Meta:
+        model = Policy
+        fields = ['id', 'name', 'members', 'url']
