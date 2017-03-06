12318ebfa93ce41c54dca4ee7f4fdcba042e73e2
Calin Don <calin.don@gmail.com>
2017-03-06 12:29:01 +0200

    Made JSONRenderer as default renderer

--- zinc/settings/base.py
+++ zinc/settings/base.py
@@ -177,8 +177,8 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     ),
     'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
