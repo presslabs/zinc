dbb2cf22cfb66c0375f64efdf3e6e13f363302fc
Calin Don <calin.don@gmail.com>
2017-03-08 18:54:05 +0200

    Fixed docker-entrypoint after refactor

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 LOG_LEVEL=${ZINC_LOG_LEVEL:-INFO}
 
 case "$1" in
-    "web")         exec su-exec zinc gunicorn zinc.wsgi --bind 0.0.0.0:8000;;
-    "celery")      exec su-exec zinc celery -A zinc worker --concurrency=1;;
-    "celerybeat")  exec su-exec zinc celery -A zinc beat;;
+    "web")         exec su-exec zinc gunicorn django_project.wsgi --bind 0.0.0.0:8000;;
+    "celery")      exec su-exec zinc celery -A django_project worker --concurrency=1;;
+    "celerybeat")  exec su-exec zinc celery -A django_project zinc beat;;
 esac
 
 exec "$@"
