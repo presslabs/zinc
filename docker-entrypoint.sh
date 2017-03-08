577e37c7c7597638607837a7e313ecc77b7820cd
Calin Don <calin.don@gmail.com>
2017-03-08 19:04:21 +0200

    Fix celery beat in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ LOG_LEVEL=${ZINC_LOG_LEVEL:-INFO}
 case "$1" in
     "web")         exec su-exec zinc gunicorn django_project.wsgi --bind 0.0.0.0:8000;;
     "celery")      exec su-exec zinc celery -A django_project worker --concurrency=1;;
-    "celerybeat")  exec su-exec zinc celery -A django_project zinc beat;;
+    "celerybeat")  exec su-exec zinc celery -A django_project beat;;
 esac
 
 exec "$@"
