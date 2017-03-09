96fbcc6d3814f8289d1f18257861b00fd1512729
Radu Ciorba <rciorba@users.noreply.github.com>
2017-03-09 15:21:34 +0200

    Ensure lattice_sync refuses to delete all IPs (closes #176) (#180)
    * Ensure lattice_sync refuses to delete all IPs
    * update tests
    * remove factory-boy, use only DDF

--- lattice_sync/tests/test_sync.py
+++ lattice_sync/tests/test_sync.py
@@ -1,29 +1,45 @@
+import json
+
 import pytest
 import responses
+from django_dynamic_fixture import G
 
-from zinc.models import IP
-from tests.fixtures import boto_client  # noqa: F401
-
-from factories.dns.ip_factory import IPFactory
 from lattice_sync import sync
+from tests.fixtures import boto_client  # noqa: F401
+from zinc.models import IP
 
 lattice = sync.lattice_factory(url='http://lattice', user='user', password='password')
 
 
 @pytest.mark.django_db
 @responses.activate
-def test_resets_existing_ips_on_run(boto_client):
+def test_wont_delete_all_ips(boto_client):
     for url in ['http://lattice/servers/', 'http://lattice/datacenters/']:
         responses.add(responses.GET, url,
                       body='[]',
                       content_type='application/json')
 
     addr = '123.123.123.123'
-    IPFactory(ip=addr)
+    G(IP, ip=addr)
+
+    assert list(IP.objects.all().values_list('ip', flat=True)) == [addr]
+    with pytest.raises(AssertionError) as excp_info:
+        sync.sync(lattice)
+    assert excp_info.match("Refusing to delete all IPs")
+    assert list(IP.objects.values_list('ip', flat=True)) == [addr]
+
+
+@pytest.mark.django_db
+@responses.activate
+def test_removes_ip(boto_client):
+    _mock_lattice_responses()
+
+    addr = '1.2.3.4'  # not in the mock response
+    G(IP, ip='1.2.3.4')
 
     assert list(IP.objects.all().values_list('ip', flat=True)) == [addr]
     sync.sync(lattice)
-    assert IP.objects.filter(ip=addr).count() == 0
+    assert not IP.objects.filter(ip=addr).exists()
 
 
 @pytest.mark.django_db
@@ -60,79 +76,84 @@ def test_fields_on_written_ip(boto_client):
 
 
 def _mock_lattice_responses():
-    servers_payload = '''
-    [{
-    "hostname": "a.presslabs.net",
-    "state": "configured",
-    "group": "",
-    "environment": "production",
-    "roles": [
-        "edge-node"
-    ],
-    "datacenter_name": "AMS1",
-    "service_id": "",
-    "ips": [{
-        "ip": "123.123.123.123",
-        "netmask": "",
-        "gateway": "",
-        "url": "http://localhost:8001/servers/a/ips/123.123.123.123?format=json",
-        "description": ""},
+    servers_payload = [
         {
-            "ip": "123.123.123.124",
-            "netmask": "",
-            "gateway": "",
-            "url": "http://localhost:8001/servers/a/ips/123.123.123.124?format=json"
-        }],
-    "uplink_speed": null,
-    "bandwidth": null,
-    "memory": null,
-    "cpu_model_name": "",
-    "cpu_model_url": null,
-    "datacenter_url": "http://localhost:8001/datacenters/5?format=json"},
+            "hostname": "a.presslabs.net",
+            "state": "configured",
+            "group": "",
+            "environment": "production",
+            "roles": [
+                "edge-node"
+            ],
+            "datacenter_name": "AMS1",
+            "service_id": "",
+            "ips": [
+                {
+                    "ip": "123.123.123.123",
+                    "netmask": "",
+                    "gateway": "",
+                    "url": "http://localhost:8001/servers/a/ips/123.123.123.123?format=json",
+                    "description": ""
+                },
+                {
+                    "ip": "123.123.123.124",
+                    "netmask": "",
+                    "gateway": "",
+                    "url": "http://localhost:8001/servers/a/ips/123.123.123.124?format=json"
+                }],
+            "uplink_speed": None,
+            "bandwidth": None,
+            "memory": None,
+            "cpu_model_name": "",
+            "cpu_model_url": None,
+            "datacenter_url": "http://localhost:8001/datacenters/5?format=json"
+        },
+        {
+            "hostname": "b",
+            "state": "configured",
+            "group": "",
+            "environment": "production",
+            "roles": [
+                "random-node"
+            ],
+            "datacenter_name": "AMS2",
+            "service_id": "",
+            "ips": [{
+                "ip": "123.123.123.125",
+                "netmask": "",
+                "gateway": "",
+                "url": "http://localhost:8001/servers/b/ips/123.123.123.125?format=json",
+                "description": ""
+            }],
+            "uplink_speed": None,
+            "bandwidth": None,
+            "memory": None,
+            "cpu_model_name": "",
+            "cpu_model_url": None,
+            "datacenter_url": "http://localhost:8001/datacenters/6?format=json"
+        }]
 
-    {
-    "hostname": "b",
-    "state": "configured",
-    "group": "",
-    "environment": "production",
-    "roles": [
-        "random-node"
-    ],
-    "datacenter_name": "AMS2",
-    "service_id": "",
-    "ips": [{
-        "ip": "123.123.123.125",
-        "netmask": "",
-        "gateway": "",
-        "url": "http://localhost:8001/servers/b/ips/123.123.123.125?format=json",
-        "description": ""}],
-    "uplink_speed": null,
-    "bandwidth": null,
-    "memory": null,
-    "cpu_model_name": "",
-    "cpu_model_url": null,
-    "datacenter_url": "http://localhost:8001/datacenters/6?format=json"}]'''
-
-    datacenters_payload = '''[{
-        "name": "AMS1",
-        "location": "Amsterdam, NL",
-        "provider": "providers.providers.DigitalOcean",
-        "latitude": null,
-        "longitude": null,
-        "id": 5
-    },
-    {
-        "name": "AMS2",
-        "location": "Amsterdam, NL",
-        "provider": "providers.providers.DigitalOcean",
-        "latitude": null,
-        "longitude": null,
-        "id": 6
-    }]'''
+    datacenters_payload = [
+        {
+            "name": "AMS1",
+            "location": "Amsterdam, NL",
+            "provider": "providers.providers.DigitalOcean",
+            "latitude": None,
+            "longitude": None,
+            "id": 5
+        },
+        {
+            "name": "AMS2",
+            "location": "Amsterdam, NL",
+            "provider": "providers.providers.DigitalOcean",
+            "latitude": None,
+            "longitude": None,
+            "id": 6
+        }]
 
     responses.add(responses.GET, 'http://lattice/servers/',
-                  body=servers_payload,
+                  body=json.dumps(servers_payload),
                   content_type='application/json')
     responses.add(responses.GET, 'http://lattice/datacenters/',
-                  body=datacenters_payload,
+                  body=json.dumps(datacenters_payload),
                   content_type='application/json')
