ee38028c3c9069080628998eff5d23d9f0db3cd9
Radu Ciorba <radu@devrandom.ro>
2017-03-13 16:40:59 +0200

    fix extraction of NS from r53
    fixes #182

--- zinc/ns_check.py
+++ zinc/ns_check.py
@@ -1,6 +1,7 @@
+from django.conf import settings
+
 from dns.resolver import Resolver
 from dns.exception import DNSException
-from django.conf import settings
 
 
 class CouldNotResolve(Exception):
@@ -16,9 +17,7 @@ def get_resolver():
 def is_ns_propagated(zone, resolver=None):
     if resolver is None:
         resolver = get_resolver()
-    r53_name_servers = set([
-        record['values'][0] for record in zone.route53_zone.records().values()
-        if record['type'] == 'NS'])
+    r53_name_servers = set(zone.route53_zone.ns['values'])
     try:
         name_servers = set([str(ns) for ns in resolver.query(zone.root, 'NS')])
     except DNSException as e:
