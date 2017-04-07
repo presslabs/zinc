353cf9e686add09d8ca4aa427e0932ae0857a830
Radu Ciorba <radu@devrandom.ro>
2017-04-07 19:07:30 +0300

    add dockerize to image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,27 @@ set -eo pipefail
 
 LOG_LEVEL=${ZINC_LOG_LEVEL:-INFO}
 
+DOCKERIZE=""
+
+prefix="dockerize"
+
+if [[ "$1" == $prefix ]];
+then
+    DOCKERIZE="dockerize"
+    shift
+    while [[ "$1" != '--' ]] ;
+    do
+        DOCKERIZE="$DOCKERIZE $1"
+        shift
+    done
+    shift
+fi
+
+
 case "$1" in
-    "web")         exec su-exec zinc gunicorn django_project.wsgi --bind "$ZINC_WEB_ADDRESS";;
-    "celery")      exec su-exec zinc celery -A django_project worker --concurrency=4;;
-    "celerybeat")  exec su-exec zinc celery -A django_project beat;;
+    "web")         exec $DOCKERIZE su-exec zinc gunicorn django_project.wsgi --bind "$ZINC_WEB_ADDRESS";;
+    "celery")      exec $DOCKERIZE su-exec zinc celery -A django_project worker --concurrency=4;;
+    "celerybeat")  exec $DOCKERIZE su-exec zinc celery -A django_project beat;;
 esac
 
 exec "$@"
